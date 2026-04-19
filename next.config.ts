eebaf29452d020ab7bc68ab9b7d4e191d05bcf3f
Sérgio <sergio@example.com>
2026-04-19 12:56:23 -0300

    Fix: set images.unoptimized to true for static export

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,7 @@ const nextConfig: NextConfig = {
   },
   // Allow access to remote image placeholder.
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
